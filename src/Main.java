4ee0072419b8fd75b150d8641daf7c8439d00cdd
AJAagaard <augustjaagaard@gmail.com>
2024-02-08 10:54:01 +0100

    cursed commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Nej dig");
         //dette er williams push
-        //Git Gud kiddo
     }
 }
